f294a23ab2ed461913342b81a6f3a9d6ae9d3739
Mario Korban <mariokorban@outlook.com>
2018-06-27 12:42:54 +0300

    Adding Views in ejs and configuring server to get index.ejs and setting view ebgine to ejs

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ import path from 'path';
 import express from 'express';
 const server = express(); //creating a server with express
 
+//setting the view engine to be EJS
+server.set('view engine','ejs');
+
+//test rendering index.ejs located in ./views and sending content
+server.get('/',(req,res) => {
+    res.render('index', {
+        content: '...'
+   });
+});
+
 //automatically serve static assets present in public 
 server.use(express.static('public'));
 
